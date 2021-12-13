66f9ca1142a1d2494e630d8ae5d9ee3999c7913d
Boughzala Ghassen <ghassen.boughzala@esprit.tn>
2021-12-13 17:55:09 +0100

    Register done v2

--- API/controllers/authcontroller.js
+++ API/controllers/authcontroller.js
@@ -9,13 +9,14 @@ const { errorHandler } = require('../helpers/dbErrorHandling');
 
 const sgMail = require('@sendgrid/mail');
 //sgMail.setApiKey(process.env.MAIL_KEY);
-sgMail.setApiKey('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');
+//sgMail.setApiKey('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');
 
 
 
 exports.registerController = (req, res) => {
   const { name, email, password } = req.body;
   const errors = validationResult(req);
+  const { token } = req.body;
 
   if (!errors.isEmpty()) {
     const firstError = errors.array().map(error => error.msg)[0];
@@ -45,81 +46,55 @@ exports.registerController = (req, res) => {
       }
     );
 
-    const emailData = {
-      from: process.env.EMAIL_FROM,
-      to: email,
-      subject: 'Account activation link',
-      html: `
-                <h1>Please use the following to activate your account</h1>
-                <p>${process.env.CLIENT_URL}/users/activate/${token}</p>
-                <hr />
-                <p>This email may containe sensetive information</p>
-                <p>${process.env.CLIENT_URL}</p>
-            `
-    };
-
-    sgMail
-      .send(emailData)
-      .then(sent => {
-        return res.json({
-          message: `Email has been sent to ${email}`
-        });
-      })
-      .catch(err => {
-        return res.status(400).json({
-          success: false,
-          errors: errorHandler(err)
-        });
+    if (token) {
+      jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, (err, decoded) => {
+        if (err) {
+          console.log('Activation error');
+          return res.status(401).json({
+            errors: 'Expired link. Signup again'
+          });
+        } else {
+          const { name, email, password } = jwt.decode(token);
+  
+          console.log(email);
+          const user = new User({
+            name,
+            email,
+            password
+          });
+  
+          user.save((err, user) => {
+            if (err) {
+              console.log('Save error', errorHandler(err));
+              return res.status(401).json({
+                errors: errorHandler(err)
+              });
+            } else {
+              return res.json({
+                success: true,
+                message: user,
+                message: 'Signup success'
+              });
+            }
+          });
+        }
       });
-  }
-};
+    } else {
+      return res.json({
+        message: 'error happening please try again'
+      });
+    }
 
-exports.activationController = (req, res) => {
-  const { token } = req.body;
 
-  if (token) {
-    jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, (err, decoded) => {
-      if (err) {
-        console.log('Activation error');
-        return res.status(401).json({
-          errors: 'Expired link. Signup again'
-        });
-      } else {
-        const { name, email, password } = jwt.decode(token);
 
-        console.log(email);
-        const user = new User({
-          name,
-          email,
-          password
-        });
 
-        user.save((err, user) => {
-          if (err) {
-            console.log('Save error', errorHandler(err));
-            return res.status(401).json({
-              errors: errorHandler(err)
-            });
-          } else {
-            return res.json({
-              success: true,
-              message: user,
-              message: 'Signup success'
-            });
-          }
-        });
-      }
-    });
-  } else {
-    return res.json({
-      message: 'error happening please try again'
-    });
   }
 };
 
 exports.signinController = (req, res) => {
   const { email, password } = req.body;
   const errors = validationResult(req);
+  
   if (!errors.isEmpty()) {
     const firstError = errors.array().map(error => error.msg)[0];
     return res.status(422).json({
