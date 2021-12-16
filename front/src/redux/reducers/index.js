160e0c72dc3f2c979c5ccf0759927f63ad199e32
Boughzala Ghassen <ghassen.boughzala@esprit.tn>
2021-12-16 01:33:45 +0100

    Redux Front

---
+++ front/src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+//import itemReducer from './itemReducer';
+import errorReducer from './errorReducer';
+import authReducer from './authReducer';
+
+export default combineReducers({
+  //item: itemReducer,
+  error: errorReducer,
+  auth: authReducer
+});
