c8da59cf769edcaed9c4e1f337c9a3598bc696bc
Boughzala Ghassen <ghassen.boughzala@esprit.tn>
2022-02-13 15:21:50 +0100

    update server.js

--- server.js
+++ server.js
@@ -37,9 +37,9 @@ app.use("/api/orders", orderRoute);
 app.use("/api/fournisseurs", fournisseurRoute);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client-admin/build'))
+    app.use(express.static('client-admin/build'))
     app.get('*', (req, res)=> {
-        res.sendFile(path.resolve(__dirname + '../','client-admin', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname ,'client-admin', 'build', 'index.html'));
 
     })
 }
