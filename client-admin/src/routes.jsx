5d61b7a1c0d9684d064e8677ddc3639bda2d8171
Ghassen Boughzala <ghassen.boughzala@esprit.tn>
2022-09-17 13:36:06 +0100

    Update project

--- client-admin/src/routes.jsx
+++ client-admin/src/routes.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 //import Navbar from "./components/navbar/navbar.component";
