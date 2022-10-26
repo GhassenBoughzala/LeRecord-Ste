7ddd73e49c28c333c74d412a5df880876d37f374
Boughzala Ghassen <ghassen.boughzala@esprit.tn>
2022-10-26 09:35:47 +0100

    Pagination fixed

--- client-admin/src/helpers/pagination.js
+++ client-admin/src/helpers/pagination.js
@@ -24,7 +24,7 @@ const PaginationComponent = ({
           <a
             href="#"
             aria-current="page"
-            class="bg-blue-50 border border-gray-300 text-blue-600 hover:bg-blue-100 hover:text-blue-700  py-2 px-3 dark:border-gray-700 dark:bg-gray-700 dark:text-white no-underline"
+            className="bg-blue-50 border border-gray-300 text-blue-600 hover:bg-blue-100 hover:text-blue-700  py-2 px-3 dark:border-gray-700 dark:bg-gray-700 dark:text-white no-underline"
           >
             {i}
           </a>
@@ -38,16 +38,16 @@ const PaginationComponent = ({
   if (totalPages === 0) return null;
 
   return (
-    <div class="max-w-2xl mx-auto my-10">
+    <div className="max-w-2xl mx-auto my-10">
       <nav aria-label="Page navigation example">
-        <ul class="inline-flex -space-x-px">
+        <ul className="inline-flex -space-x-px">
           <li disabled={currentPage === 1}>
             <a
               onClick={() => onPageChange(currentPage - 1)}
               href="#"
-              class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 ml-0 rounded-l-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              className="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 ml-0 rounded-l-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             >
-              <i class="fas fa-angle-left"></i>
+              <i className="fas fa-angle-left"></i>
             </a>
           </li>
 
@@ -56,9 +56,9 @@ const PaginationComponent = ({
             <a
               onClick={() => onPageChange(currentPage + 1)}
               href="#"
-              class="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 rounded-r-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              className="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 rounded-r-lg leading-tight py-2 px-3 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             >
-              <i class="fas fa-angle-right"></i>
+              <i className="fas fa-angle-right"></i>
             </a>
           </li>
         </ul>
