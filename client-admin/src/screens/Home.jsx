05222da5bc0439b78452ae2e8de9e21f9f431c22
Boughzala Ghassen <ghassen.boughzala@esprit.tn>
2022-11-02 13:11:35 +0100

    Couverture home page

--- client-admin/src/screens/Home.jsx
+++ client-admin/src/screens/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "../components/container/container.component";
 import Footer from "../components/home/Footer";
 import RBG from "../assests/img/MoyR.png";
-import COTU from "../assests/img/cotubg.jpg";
+import COTU from "../assests/img/couverture-articles-01.png";
 
 import Fournisseur from "../components/home/Fournisseur";
 import Fourniture from "../components/home/Fourniture";
@@ -15,35 +15,33 @@ const Home = () => {
       <Container>
         <Navbar />
         <br />
-        <section className="w-full px-6 pb-12 antialiased bg-white">
+        <section className="w-full pb-12 antialiased bg-white">
           <div className="mx-auto max-w-7xl">
             <div className="container max-w-lg px-4 py-12 mx-auto text-left md:max-w-none md:text-center">
               <h1 className="text-5xl font-extrabold leading-10 tracking-tight text-left text-gray-900 md:text-center sm:leading-none md:text-6xl lg:text-7xl">
                 <span className="inline md:block"></span>
-                <span className="relative mt-2 text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 to-indigo-500 md:inline-block">
+                <span className="relative text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 to-indigo-500 md:inline-block">
                   <img src={RBG} alt="" />
                 </span>
+                <div className="m-0 text-xl font-semibold leading-tight lg:text-2xl md:text-2xl pt-6">
+                  Nous avons l’honneur de tenir cette opportunité pour nous
+                  présenter brièvement notre société{" "}
+                </div>
+              
+                <div className="m-0 text-xl font-semibold leading-tight lg:text-2xl md:text-2xl pt-6">
+                  Nous « Le Record », sommes spécialisé dans la
+                  commercialisation de fournitures de confection
+                </div>
               </h1>
-              <br />
-              <br />
-              <div className="m-0 text-xl font-normal leading-tight border-0 border-blue-300 lg:text-2xl md:text-2xl">
-                Nous avons l’honneur de tenir cette opportunité pour nous
-                présenter brièvement notre société{" "}
-              </div>
-              <br />
-              <div className="m-0 text-xl font-normal leading-tight border-0 border-blue-300 lg:text-2xl md:text-2xl">
-                Nous « Le Record », sommes spécialisé dans la commercialisation
-                de fournitures de confection
-              </div>
             </div>
             <div className="w-full md:text-center">
-              <div className="w-full overflow-hidden rounded-md shadow-xl sm:rounded-xl">
+              <div className="w-full overflow-hidden rounded-md sm:rounded-xl pb-12">
                 <img src={COTU} alt="" />
               </div>
             </div>
           </div>
         </section>
-
+        
         <Fournisseur />
         <Fourniture />
         <Gamme />
