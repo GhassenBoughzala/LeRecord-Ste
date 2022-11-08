6f13d9db3f9abb90588b2e5debe8c40e243bcced
Boughzala Ghassen <ghassen.boughzala@esprit.tn>
2022-11-08 13:22:25 +0100

    Update project

--- backend/config/dbconfig.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    secret : 'secret',
-    database : 'mongodb+srv://RR:2021@cluster0.hysuw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
--- backend/routes/devis.js
+++
@@ -1,4 +0,0 @@
-const devis = require("../models/Devis");
-const auth = require("../middleware/auth");
-const router = require("express").Router();
-
