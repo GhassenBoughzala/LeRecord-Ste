872421fd072d639716ec81e31b1445869b1cc47c
Boughzala Ghassen <ghassen.boughzala@esprit.tn>
2022-11-10 10:36:33 +0100

    Fixing not found after deploy

--- server.js
+++ server.js
@@ -37,10 +37,10 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/fournisseurs", fournisseurRoute);
 
- if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client", "./build")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "./client", "./build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
