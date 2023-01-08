8381bade6f6f4ef80f267ace5c7394897a590f9f
Boughzala Ghassen <ghassen.boughzala@esprit.tn>
2023-01-08 12:37:57 +0100

    Fix product by id

--- middleware/productById.js
+++ middleware/productById.js
@@ -1,5 +1,5 @@
 const mongoose  = require ('mongoose');
-const Product = require('../models/Product');
+const Product = require('../models/ProductLow');
 
 module.exports = async function(req, res, next ){
 
