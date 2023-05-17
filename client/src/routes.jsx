8b7aaaa6a86276686a5c7c8cacb600baeccbe530
Boughzala Ghassen <ghassen.boughzala@esprit.tn>
2023-05-17 13:10:58 +0100

    Update routes

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Home from "./screens/Home";
 import Magasin from "./screens/Magasin";
 
 const Routes = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <ToastContainer position="bottom-right" />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/accueil" component={Home} />
         <Route exact path="/catalogue" component={Magasin} />
       </Switch>
-    </BrowserRouter>
+    </Router>
   );
 };
 
