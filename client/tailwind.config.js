79be3881044e04c06e96805d2dcea67ee5dc4d49
Ghassen Boughzala <ghassen.boughzala@esprit.tn>
2024-05-22 13:03:24 +0100

    Add DaisyUi package

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  plugins: [require('daisyui')],
+};
