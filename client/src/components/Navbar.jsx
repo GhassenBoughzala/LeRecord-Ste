2eff3dddc29a71cbb57f65eb31914e2e4082d6d3
Ghassen Boughzala <ghassen.boughzala@esprit.tn>
2024-05-25 13:05:55 +0100

    Fix drawer & add theme

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,135 +1,83 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Rimg from "../assests/r.png";
+import Theme from "./Theme";
 
 export const Navbar = () => {
-  const [theme, setTheme] = useState(
-    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
-  );
-
-  const toggleTheme = (e) => {
-    if (e.target.checked) {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-  };
-
-  useEffect(() => {
-    localStorage.setItem("theme", theme);
-    const locaclTheme = localStorage.getItem("theme");
-    document.querySelector("html").setAttribute("data-theme", locaclTheme);
-  }, [theme]);
-
   return (
     <>
-      <div class="drawer">
-        <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
-        <div class="drawer-content flex flex-col">
-          {/* <!-- Navbar --> */}
-          <div class="w-full navbar bg-base-300">
-            <div class="flex-none lg:hidden">
+      <div className="drawer">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          {/* Navbar */}
+          <div className="w-full navbar bg-base-300">
+            <div className="flex-none lg:hidden">
               <label
-                for="my-drawer-3"
+                htmlFor="my-drawer-3"
                 aria-label="open sidebar"
-                class="btn btn-square btn-ghost"
+                className="btn btn-square btn-ghost"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  class="inline-block w-6 h-6 stroke-current"
+                  className="inline-block w-6 h-6 stroke-current"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M4 6h16M4 12h16M4 18h16"
                   ></path>
                 </svg>
               </label>
             </div>
-            <div className="flex-1">
+            <div className="flex-1 px-2 mx-2">
               <Link to="/accueil" className="logo w-40 ">
                 <img src={Rimg} alt="" />
               </Link>
             </div>
-            <div className="flex-none">
-              <ul className="menu menu-horizontal px-1">
+            <div className="flex-none hidden lg:block">
+              <ul className="menu menu-horizontal">
+                {/* Navbar menu content here */}
+                <li>
+                  <Link to="/acceuil">
+                    <b className=" text-blue-900">Acceuil</b>
+                  </Link>
+                </li>
                 <li>
                   <Link to="/catalogue">
                     <b className=" text-blue-900">Catalogue</b>
                   </Link>
                 </li>
                 <li>
-                  <details>
-                    <summary>User</summary>
-                    <ul className="p-2 bg-base-100 rounded-t-none">
-                      <li>
-                        <a>Link 1</a>
-                      </li>
-                      <li>
-                        <a>Link 2</a>
-                      </li>
-                    </ul>
-                  </details>
+                  <Theme />
                 </li>
               </ul>
-              <label className="swap swap-rotate px-2">
-                {/* this hidden checkbox controls the state */}
-                <input
-                  type="checkbox"
-                  onChange={toggleTheme}
-                  checked={theme === "light" ? false : true}
-                />
-
-                {/* sun icon */}
-                <svg
-                  className="swap-on fill-current w-6 h-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-                </svg>
-
-                {/* moon icon */}
-                <svg
-                  className="swap-off fill-current w-6 h-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-                </svg>
-              </label>
             </div>
           </div>
         </div>
-        <div class="drawer-side">
+        <div className="drawer-side">
           <label
-            for="my-drawer-3"
+            htmlFor="my-drawer-3"
             aria-label="close sidebar"
-            class="drawer-overlay"
+            className="drawer-overlay"
           ></label>
-          <ul class="menu p-4 w-80 min-h-full bg-base-200">
-            {/*  <!-- Sidebar content here --> */}
+          <ul className="menu p-4 w-80 min-h-full bg-base-200">
+            {/* Sidebar content here */}
+            <li>
+              <Link to="/acceuil">
+                <b className=" text-blue-900">Acceuil</b>
+              </Link>
+            </li>
             <li>
               <Link to="/catalogue">
                 <b className=" text-blue-900">Catalogue</b>
               </Link>
             </li>
             <li>
-              <details>
-                <summary>User</summary>
-                <ul className="p-2 bg-base-100 rounded-t-none">
-                  <li>
-                    <a>Link 1</a>
-                  </li>
-                  <li>
-                    <a>Link 2</a>
-                  </li>
-                </ul>
-              </details>
+              <Theme />
             </li>
           </ul>
         </div>
